8daff8bd822209fb43bd62dfa3df546bf21c585e
Faishal Saif <faishalsaif331@gmail.com>
2023-02-15 21:30:42 +0530

    added files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import './App.css'
 
-function App() {
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  const [color , setColor] = useState("black")
+
+  useEffect(() => {
+    (count >= 0 && count <= 4 && setColor("green"));
+    (count >= 5 && count <= 9 && setColor("blue"));
+    (count === 10 && setColor("red"));
+  }, [count])
+
+  const handleInc = ()=>{
+    (count < 10 && setCount(count+1)); 
+
+  }
+
+  const handleDec = () =>{
+    (count > 0 && setCount(count-1));
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Counter Application</h1>
+      <h1 style={{
+        color: color,
+        fontSize:"60px"
+      }}>{count}</h1>
+      <button className="btn" onClick={()=>{handleInc()}}>Increment</button>
+      <button className="btn" onClick={()=>{handleDec()}}>Decrement</button>
     </div>
-  );
+  )
 }
-
-export default App;
